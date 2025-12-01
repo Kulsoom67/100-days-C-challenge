ab77265f3db09c0599cf169619c22e0ef8b43bed
Kulsoom <kulsoom20011@gmail.com>
2025-12-01 10:58:21 +0530

    Update and rename Day 19 to day19.q37.c

--- day19.q37.c
+++ day19.q37.c
@@ -19,29 +19,10 @@ int main() {
 
     lcm = (num1 * num2) / gcd(num1, num2);
 
-    // Output the result
+  
     printf("%d\n", lcm);
 
     return 0;
 }
 
 
-Q38: Write a program to find the sum of digits of a number.
-
-#include <stdio.h>
-
-int main() {
-    int number, sum = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    while (number > 0) {
-        sum += number % 10;  // Add last digit to sum
-        number = number / 10;  // Remove last digit
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
-}
