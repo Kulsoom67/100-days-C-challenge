afb3f606836bb01230fc29a1b1a28c645d9685ea
Kulsoom <kulsoom20011@gmail.com>
2025-12-01 23:25:31 +0530

    Update and rename Day 42 to day42.q83.c

--- day42.q83.c
+++ day42.q83.c
@@ -8,8 +8,7 @@ int main() {
     int i = 0;
 
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); // Reads string including spaces
-
+    fgets(str, sizeof(str), stdin);
     while (str[i] != '\0') {
         char ch = str[i];
         if (ch >= 'A' && ch <= 'Z') {
@@ -26,29 +25,6 @@ int main() {
     }
 
     printf("Vowels=%d, Consonants=%d\n", vowels, consonants);
-
-    return 0;
-}
-
-Q84: Convert a lowercase string to uppercase without using built-in functions.
-
-#include <stdio.h>
-
-int main() {
-    char str[1000];
-    int i = 0;
-
-    printf("Enter a lowercase string: ");
-    fgets(str, sizeof(str), stdin); 
-
-    while (str[i] != '\0') {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - 32; 
-        }
-        i++;
-    }
-
-    printf("Uppercase string: %s", str);
 
     return 0;
 }
